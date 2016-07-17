ff9f51da87217815dfc352e7329d1a83388c4f80
Matheus Rocha Mendes <omegamendes@Matheuss-MacBook-Pro.local>
2016-07-17 17:52:16 -0300

    First

---
+++ src/main/java/com/omegamendes/dash/rest/Dota2API.java
@@ -0,0 +1,7 @@
+package com.omegamendes.dash.rest;
+
+/**
+ * Created by omegamendes on 7/17/16.
+ */
+public class Dota2API {
+}
---
+++ src/main/java/com/omegamendes/dash/rest/RestApiCreator.java
@@ -0,0 +1,11 @@
+package com.omegamendes.dash.rest;
+
+/**
+ * Created by omegamendes on 7/17/16.
+ */
+public class RestApiCreator {
+    
+    public static Dota2API dota2API() {
+        return null;
+    }
+}
