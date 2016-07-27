eb7e340692cf8d2496c645aef5b8a8cc247120bf
Matheus Mendes <matheusrochamendes@gmail.com>
2016-07-27 13:50:37 -0300

    imagereader

---
+++ src/test/java/com/omegamendes/dash/rest/ImageReaderTest.java
@@ -0,0 +1,14 @@
+package com.omegamendes.dash.rest;
+
+import org.junit.Test;
+
+/**
+ * Created by omegamendes on 7/27/16.
+ */
+public class ImageReaderTest {
+    
+    @Test
+    public void imageRead() {
+        
+    }
+}
